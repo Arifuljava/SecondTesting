663328bdc0d76cfba65a949dbab2d74b21fc284c
Ariful0007 <arifulpub143@gmail.com>
2023-05-21 12:26:04 +0600

    bbbbbb cv

--- SecondTesting/ViewController.swift
+++ SecondTesting/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableview: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
